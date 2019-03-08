f9647a0bd6fa42ddac4a8da62f75350963cb6fc8
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2019-03-07 17:40:35 -0800

    PDS-771: Update feedback widget to call new endpoint

--- feedback/js/feedback.js
+++ feedback/js/feedback.js
@@ -5,6 +5,10 @@
 
  Released under MIT License
 */
+document.addEventListener("DOMContentLoaded", function(){
+	Feedback();
+});
+
 (function( window, document, undefined ) {
 if ( window.Feedback !== undefined ) {
     return;
@@ -74,7 +78,7 @@ scriptLoader = function( script, func ){
 sendButton,
 HOST = "https://pds-gamma.jpl.nasa.gov",
 captchaUrl = "/feedback/recaptcha-v3-verify.php",
-feedbackUrl = "/email-service/EmailSendingServlet",
+feedbackUrl = "/email-service/SubmitFeedback",
 modal = document.createElement("div"),
 modalBody = document.createElement("div"),
 modalHeader = document.createElement("div"),

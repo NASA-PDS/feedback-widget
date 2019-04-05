805f8d495491c62ddf6225d851c9562e72e9e6e1
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2019-04-05 13:28:11 -0700

    Updates for error checking and configuration
    Ported from the portal repo, these updates include changes to resolve
    PDS-751, PDS-784, PDS-798, PDS-784, PDS-647, and PDS-806.
    
    Changes include:
    * Make Feedback comments required
    * Improved error handling when recaptcha or email send fails
    * Configurable tab size
    * Configurable tab color

---
+++ feedback/js/config.js
@@ -0,0 +1,10 @@
+var config = {
+	tab: {
+		color: "",		// default: NASA blue.
+		fontColor: "",	// default: white.
+		size: {
+			width: "",	// default: 115px.
+			height: ""	// default: 40px.
+		}
+	}
+};

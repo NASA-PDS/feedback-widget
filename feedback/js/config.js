6eb8a6fc1d42369b91fb74429a09194e4c6e5a21
Catherine Suh <catherine.e.suh@jpl.nasa.gov>
2019-04-11 20:33:43 -0700

    PDS-819: Update application with configurable strings. (#6)

--- feedback/js/config.js
+++ feedback/js/config.js
@@ -1,4 +1,5 @@
 var config = {
+	host: "https://pds.nasa.gov",		// REQUIRED. no default value.
 	tab: {
 		label: "",			// default: Need Help?
 		color: "",			// default: NASA blue
@@ -12,6 +13,11 @@ var config = {
 	feedback: {
 		header: "",			// default: Help Desk
 		text: "",			// default: How can we help you? Send us your question or feedback and we will get back to you within the next 24 hours.
+		feedbackType: "",	// default: Comment, Question, Problem/Bug, Kudos, Other
+		sentStatus: "",		// default: Thank you for your feedback.
+		sentFollowup: "",	// default: If you provided an email address, a representative will get back to you as soon as possible.
+		errorStatus: "",	// default: There was an error sending your feedback.
+		errorFollowup: "",	// default: If the problem persists, please email <pds_operator@jpl.nasa.gov - email address is non-configurable>
 		followupGeneral: "",// default: In the meantime, you may find the following links helpful:,
 		followupLinks: ""	// default: https://pds.nasa.gov/site-help.shtml,https://pds.nasa.gov/home/users/,https://pds.nasa.gov/home/proposers/,https://pds.nasa.gov/home/providers/
 	}

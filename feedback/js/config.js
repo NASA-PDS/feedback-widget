81f4efd1b7a61ebadb21158790994be56b13ed2a
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2019-04-30 11:38:08 -0700

    Consolidate and simplify config options

--- feedback/js/config.js
+++ feedback/js/config.js
@@ -1,28 +1,22 @@
 var config = {
-	host: "https://pds.nasa.gov",		// REQUIRED. no default value.
+	host: "https://pds.nasa.gov",		// Email Server Host - should be https://pds.nasa.gov
 	tab: {
-		label: "",			// default: Need Help?
-		color: "",			// default: #0b3d91
-		fontColor: "",		// default: #ffffff
-		fontSize: "",		// default: 16px
+		label: "Need Help?",		// default: Need Help?
+		color: "#0b3d91",		// default: #0b3d91
+		fontColor: "#ffffff",		// default: #ffffff
+		fontSize: "16px",		// default: 16px
 		size: {
-			width: "",		// default: 150px
-			height: ""		// default: 60px
+			width: "150px",		// default: 150px
+			height: "60px"		// default: 60px
 		},
 		placement: {
-			side: "",		// default: right
-			offset: ""		// default: 45vh or 0vw
+			side: "right",		// default: right
+			offset: "45"		// default: 45vh or 0vw
 		}
 	},
 	feedback: {
-		header: "",			// default: Help Desk
-		text: "",			// default: How can we help you? Send us your question or feedback and we will get back to you within the next 24 hours.
-		feedbackType: "",	// default: Comment, Question, Problem/Bug, Kudos, Other
-		sentStatus: "",		// default: Thank you for your feedback.
-		sentFollowup: "",	// default: If you provided an email address, a representative will get back to you as soon as possible.
-		errorStatus: "",	// default: There was an error sending your feedback.
-		errorFollowup: "",	// default: If the problem persists, please email <pds_operator@jpl.nasa.gov - email address is non-configurable>
-		followupGeneral: "",// default: In the meantime, you may find the following links helpful:,
-		followupLinks: ""	// default: https://pds.nasa.gov/site-help.shtml,https://pds.nasa.gov/home/users/,https://pds.nasa.gov/home/proposers/,https://pds.nasa.gov/home/providers/
+		header: "Help Desk",
+		feedbackType: "Comment,Question,Problem/Bug,Kudos,Other",
+		followupLinks: "https://pds.nasa.gov/site-help.shtml,https://pds.nasa.gov/home/users/,https://pds.nasa.gov/home/proposers/,https://pds.nasa.gov/home/providers/"
 	}
 };

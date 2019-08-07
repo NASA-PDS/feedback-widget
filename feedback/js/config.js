23efd2125c22b43f14d5f9d98c3e3446be983d3f
c-suh <catherine.e.suh@jpl.nasa.gov>
2019-08-06 17:38:33 -0700

    Readme (and consequent) edits (#6)
    Address Issue #5 with the following:
    
    - Fix discrepancy between stating 'integers only' and not adhering
      to it in practice or example.
    - Explicitly state what unit is used with the 'integers only' values.
    - Simplify and explain constraints such as the closed interval for the
      offset configurable variable.
    - Expand explanation of the additionalLinks configurable variable to
      match its JSON structure.
    - Reorder the JSON syntax in config.js to mirror the order of explained
      configurable variables in the README.
    
    Additional changes:
    
    - Modularize JavaScript handling configurable variables.
    - Implement constraints on the offset configurable variable so that the
      Feedback tab is never out of the viewport.
    - Add logic to better place the Feedback tab according to its width and
      height.
    - Rename 'followupLinks' to 'additionalLinks'.

--- feedback/js/config.js
+++ feedback/js/config.js
@@ -1,43 +1,43 @@
 var config = {
-	host: "https://pds.nasa.gov",		// Email Server Host - should be https://pds.nasa.gov
-	tab: {
-		label: "Need Help?",		// default: Need Help?
-		color: "#0b3d91",		// default: #0b3d91
-		fontColor: "#ffffff",		// default: #ffffff
-		fontSize: "16px",		// default: 16px
-		size: {
-			width: "150px",		// default: 150px
-			height: "60px"		// default: 60px
-		},
-		placement: {
-			side: "right",		// default: right
-			offset: "45"		// default: 45vh or 0vw
-		}
-	},
+	host: "https://pds.nasa.gov",	// Email Server Host
 	feedback: {
 		header: "Help Desk",
 		feedbackType: "Comment,Question,Problem/Bug,Kudos,Other",
-		followupLinks: [ 
-                    { 
-                        title: "Information for Data Users",
-                        url: "https://pds.nasa.gov/home/users/"
-                    },
-                    {
-                        title: "Information for Proposers",
-                        url: "https://pds.nasa.gov/home/proposers/"
-                    },
-                    {
-                        title: "Information for Providers",
-                        url: "https://pds.nasa.gov/home/providers/"
-                    },
-                    { 
-                        title: "OpenPlanetary Forum", 
-                        url: "https://forum.openplanetary.org/"
-                    },
-                    { 
-                        title: "Site Map", 
-                        url: "https://pds.nasa.gov/site-help.shtml" 
-                    }
+		additionalLinks: [
+			{
+				title: "Information for Data Users",
+				url: "https://pds.nasa.gov/home/users/"
+			},
+			{
+				title: "Information for Proposers",
+				url: "https://pds.nasa.gov/home/proposers/"
+			},
+			{
+				title: "Information for Providers",
+				url: "https://pds.nasa.gov/home/providers/"
+			},
+			{
+				title: "OpenPlanetary Forum",
+				url: "https://forum.openplanetary.org/"
+			},
+			{
+				title: "Site Map",
+				url: "https://pds.nasa.gov/site-help.shtml"
+			}
 		]
+	},
+	tab: {
+		label: "Need Help?",		// default: Need Help?
+		color: "#0b3d91",			// default: #0b3d91
+		fontColor: "#ffffff",		// default: #ffffff
+		fontSize: "16",				// default: 16
+		size: {
+			width: "150",			// default: 150
+			height: "60"			// default: 60
+		},
+		placement: {
+			side: "right",			// default: right
+			offset: "50"			// default: 50
+		}
 	}
 };
